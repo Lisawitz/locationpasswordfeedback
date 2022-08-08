7d5d69388ee3f6f31b674189e7a597e3a65c97f6
Lisawitz <51464286+Lisawitz@users.noreply.github.com>
2022-08-07 18:20:22 -0700

    Add files via upload

---
+++ public/pwmeter.js
@@ -0,0 +1,18 @@
+/*$(document).ready(function () {
+    "use strict";
+    var options = {};
+    options.ui = {
+        bootstrap3: true,
+        container: "#pwd-container",
+        viewports: {
+            progress: ".pwstrength_viewport_progress",
+            verdict: ".pwstrength_viewport_verdict"
+        }
+    };
+    options.common = {
+        onKeyUp: function (evt, data) {
+            $('input[name="hiddeninput"]').val(data.score);
+        }
+    }
+    $(':password').pwstrength(options);
+});*/
